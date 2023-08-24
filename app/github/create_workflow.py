9abc714f262b32e6c661efd2a3be2939ca36d65b
shay_levin <shay.levin@perimeter81.com>
2023-08-25 02:11:42 +0300

    add workflow create

--- app/github/create_workflow.py
+++ app/github/create_workflow.py
@@ -3,10 +3,13 @@ import shutil
 
 
 def create_workflow(source_file, target_root_directory):
+    print(f"github/{source_file}")
+    print(target_root_directory)
+    print(os.getcwd())
     target_directory = f"{target_root_directory}/.github/workflows"
     if not os.path.exists(target_directory):
         os.makedirs(target_directory)
-        shutil.copy(source_file, f"{target_directory}/skygen.yaml")
+        shutil.copy(f"github/{source_file}", f"{target_directory}/skygen.yaml")
         print(f"File '{source_file}' copied to '{target_directory}/skygen.yaml'.")
     else:
         print(f"File '{target_directory}' not found.")

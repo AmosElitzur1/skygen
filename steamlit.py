6ac6501b32fb381c3c299e3a86bbe06267c29543
Igor <igorr@creditguard.co.il>
2023-08-24 19:02:25 +0300

    Add parsetfvars

---
+++ steamlit.py
@@ -0,0 +1,27 @@
+import streamlit as st
+import pandas as pd
+st.text_input("Git URL")
+
+
+def parseTfvars(tfVarsFile: str) -> dict:
+    tfVars = {}
+    with open(tfVarsFile, 'r') as f:
+        for line in f.readlines():
+            if line.startswith('#') or line.startswith('\n'):
+                continue
+            else:
+                key, value = line.split('=')
+                tfVars[key.strip()] = value.replace('"', '')
+    return tfVars
+
+def click_submit_button():
+    st.session_state.submit_button_clicked = True
+
+
+if 'submit_button_clicked' not in st.session_state:
+    st.session_state.submit_button_clicked = False
+st.button("Submit", on_click=click_submit_button)
+if st.session_state.submit_button_clicked:
+    parsedTfvars = parseTfvars("demo.tfvars")
+    for key in parsedTfvars.keys():
+        st.text_input(label=key, value=parsedTfvars[key])

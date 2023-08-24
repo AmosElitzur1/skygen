722c894169f652c01b67db0e71622d15094711e7
Igor <igorr@creditguard.co.il>
2023-08-24 21:14:54 +0300

    Add find tfvars

--- steamlit.py
+++ steamlit.py
@@ -1,7 +1,19 @@
 import streamlit as st
-import pandas as pd
+import os
+
 st.text_input("Git URL")
 
+
+def find_tfvars_files(directory):
+    # List to store the file paths
+    tfvars_files = {}
+
+    # Walk through directory
+    for dirpath, dirnames, filenames in os.walk(directory):
+        for filename in filenames:
+            if filename.endswith('.tfvars'):
+                tfvars_files[filename] = os.path.join(dirpath, filename)
+    return tfvars_files
 
 def parseTfvars(tfVarsFile: str) -> dict:
     tfVars = {}

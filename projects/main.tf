d0f09176aeb801ebf3b6ff84d56498b4e5995524
amos <amos@amos.com>
2023-08-24 21:08:21 +0300

    skygen-amos

---
+++ projects/main.tf
@@ -0,0 +1,10 @@
+resource "null_resource" "test" {
+  provisioner "local-exec" {
+    command = echo "hello form ${var.name}" 
+  }
+}
+
+variable "name" {
+  type        = string
+  default     = ""
+}

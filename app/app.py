11da35490bff3e5204d9da4a9930c9b9825309b7
amos <amos@amos.com>
2023-08-24 20:58:01 +0300

    github-actions

--- app/app.py
+++ app/app.py
@@ -1,11 +1,13 @@
 import streamlit as st
 import pandas as pd
+import datetime
  
 st.write("""
 # SKYGEN TEAM!!!!
 We Are The *Champions!*
 """)
  
+st.write(f'<h2>{datetime.datetime.now()}<h2>', unsafe_allow_html=True)
 df = pd.read_csv("my_data.csv")
 st.line_chart(df)
 

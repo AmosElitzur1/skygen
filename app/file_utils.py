8d515a940c212af34a4b0bd58eae9ceccd81a86a
Igor <igorr@creditguard.co.il>
2023-08-25 02:29:42 +0300

    Add quats for tfvar value

--- app/file_utils.py
+++ app/file_utils.py
@@ -4,7 +4,7 @@ def generate_tfvars(full_path, input_tfvars, new_tfvars_file_name) -> str:
     new_path = os.path.dirname(full_path) + "/" + new_tfvars_file_name
     lines = []
     for key in input_tfvars.keys():
-        lines.append(f"{key} = {input_tfvars[key]}")
+        lines.append(f"{key} = \"{input_tfvars[key]}\"")
     str_lines = "\n".join(lines)
     with open(new_path, "w") as f:
         f.write(str_lines)

97dcc120def165811473ac63e30ac88effeceb78
Igor <igorr@creditguard.co.il>
2023-08-25 01:46:08 +0300

    Add gitpush

--- app/file_utils.py
+++ app/file_utils.py
@@ -1,6 +1,11 @@
-def generate_tfvars(input_tfvars) -> str:
+import os
+
+def generate_tfvars(full_path, input_tfvars, new_tfvars_file_name) -> str:
+    new_path = os.path.dirname(full_path) + "/" + new_tfvars_file_name
     lines = []
     for key in input_tfvars.keys():
         lines.append(f"{key} = {input_tfvars[key]}")
     str_lines = "\n".join(lines)
+    with open(new_path, "w") as f:
+        f.write(str_lines)
     return str_lines

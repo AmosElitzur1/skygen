5de18f54ae4756d4ef455aee8f6d96e6b4659ec3
Igor <igorr@creditguard.co.il>
2023-08-25 05:12:18 +0300

    Add support for .tfvars externsion

--- app/file_utils.py
+++ app/file_utils.py
@@ -1,7 +1,10 @@
 import os
 
 def generate_tfvars(full_path, input_tfvars, new_tfvars_file_name) -> str:
-    new_path = os.path.dirname(full_path) + "/" + new_tfvars_file_name
+    if os.path.splitext(new_tfvars_file_name)[1] == ".tfvars":
+        new_path = os.path.dirname(full_path) + "/" + new_tfvars_file_name
+    else:
+        new_path = os.path.dirname(full_path) + "/" + new_tfvars_file_name + ".tfvars"
     lines = []
     for key in input_tfvars.keys():
         lines.append(f"{key} = \"{input_tfvars[key]}\"")

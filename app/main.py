aa8e2bb4e0ccd4d11df542f730fb32492e78584a
Igor <igorr@creditguard.co.il>
2023-08-25 03:01:25 +0300

    Fix push

--- app/main.py
+++ app/main.py
@@ -25,7 +25,7 @@ def generate_all(selected_file, input_tfvars, new_tfvars_file_name):
     new_tfvars, full_file_path = generate_tfvars(selected_file, input_tfvars, new_tfvars_file_name)
     create_workflow("workflow_template.yaml", destination_path)
     st.header("New tfvars")
-    push_to_github(destination_path, full_file_path.replace(destination_path, ""))
+    push_to_github(destination_path, full_file_path.replace(f"{destination_path}/", ""))
     st.code(new_tfvars, language="hcl", line_numbers=True)
     st.session_state.stage = "generate_pressed"
 
